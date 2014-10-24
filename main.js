abe71b307d4bae2decb762160e452c16ddd4f045
johnharr <hajohn@live.unc.edu>
2014-10-24 13:38:50 -0400

    Added Modal function
    Added modal for about information

--- main.js
+++ main.js
@@ -7,5 +7,19 @@ EyePlay.controller('main', ['$scope', '$modal',
 
         $scope.setGame = function (g) {
             $scope.game = "htmlPartials/" + g
+        };
+
+        $scope.open = function() {
+            var modalInstance = $modal.open({
+                templateUrl: 'htmlPartials/about.html',
+                controller: 'ModalInstance'
+            });
+        }
+    }]);
+
+EyePlay.controller('ModalInstance', ['$scope', '$modalInstance',
+    function ($scope, $modalInstance) {
+        $scope.exit = function(){
+            $modalInstance.close();
         }
     }]);
